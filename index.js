b2cc861e94755e6469e728d8700bb6743f9bbf54
Prerna-Kujur <131394177+Prerna-Kujur@users.noreply.github.com>
2025-02-24 18:33:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("This is my script")
+///key=ema_live_FaS81ALnicQzf8yKrinktgyFI9NA12esMeFsS4OV
+
+let result = {
+    
+        "tag": "",
+        "free": true,
+        "role": false,
+        "user": "hello",
+        "email": "hello@gmail.com",
+        "score": 0.48,
+        "state": "undeliverable",
+        "domain": "gmail.com",
+        "reason": "invalid_mailbox",
+        "mx_found": true,
+        "catch_all": null,
+        "disposable": false,
+        "smtp_check": false,
+        "did_you_mean": "",
+        "format_valid": true
+      }
+
+
+
+
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "ema_live_FaS81ALnicQzf8yKrinktgyFI9NA12esMeFsS4OV"
+    let email = document.getElementById("username").value 
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){ 
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
+
+
